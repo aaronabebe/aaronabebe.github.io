8e0e3af0359a3874117d697cd29fe3d536ab98cc
Aaron Abebe <aaron.abebe@gmail.com>
2022-09-20 16:59:20 +0200

    updated to new website

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Aaron Abebe\'s Website';
+export const SITE_DESCRIPTION = 'Personal website, blog and contact information of Aaron Abebe';
+export const SECONDARY_COLOR = '#99FFCD';
